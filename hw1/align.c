5a1d0bfe532c9012e538764af81bcf0c582cb75f
Lin Chun Yeh <r09921093@g.ntu.edu.tw>
2023-08-07 22:17:14 +0800

    Implement align_up

--- hw1/align.c
+++ hw1/align.c
@@ -5,16 +5,16 @@ static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
     if ((alignment & mask) == 0) { /* power of two? */
-        return MMMM;
+        return sz + ((alignment - (sz & mask)) & (~alignment));
     }
     return (((sz + mask) / alignment) * alignment);
 }
 
 int main()
 {
-    printf("120, 5 : %ld\n", align_up(120, 5));
-    printf("121, 5 : %ld\n", align_up(121, 5));
-    printf("122, 5 : %ld\n", align_up(122, 5));
-    printf("123, 5 : %ld\n", align_up(123, 5));
-    printf("124, 5 : %ld\n", align_up(124, 5));
+    printf("120, 4 : %ld\n", align_up(120, 4));
+    printf("121, 4 : %ld\n", align_up(121, 4));
+    printf("122, 4 : %ld\n", align_up(122, 4));
+    printf("123, 4 : %ld\n", align_up(123, 4));
+    printf("124, 4 : %ld\n", align_up(124, 4));
 }
